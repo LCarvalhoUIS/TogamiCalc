ac1073c5c287c37d702af22ec28210e1dc33717f
LCarvalhoUIS <lucasccb@ig.com.br>
2016-08-22 15:37:56 -0500

    First commit
    Published readme and working version for Gale seeding

---
+++ TogamiCalc.py
@@ -0,0 +1,177 @@
+import re
+import sys
+import datetime
+
+#Pokemon XD RTC Calculator
+#By Lucas (/u/ItsProfOak AKA Collector Togami)
+#Credits:
+# Rising_Fog from the Dolphin dev team for making the custom RTC feature that made this possible
+#Kaphotics and Omegadonut for figuring out XDRNG PRNG
+#Zari for researching seeding
+#Admiral_Fish for helping with testing and making a video of this
+#See the readme before using this program
+
+
+print "Pokemon XD RTC Calculator 1.0 Beta"
+print "By Lucas (/u/ItsProfOak AKA Collector Togami)"
+print "Credits:"
+print "Rising_Fog from the Dolphin dev team for making the custom RTC feature that made this possible"
+print "Kaphotics and Omegadonut for figuring out XDRNG PRNG"
+print "Zari for researching seeding"
+print "Admiral_Fish for helping with testing and making a video of this"
+print "Chimera for additional testing"
+print "See the readme before using this program."
+
+
+#The calculation to move the PRNG state ahead one frame. May be modified for other games.
+def XDRNG(PRNG):
+    return(PRNG * 0x000343FD + 0x00269EC3) & 0xFFFFFFFF
+
+
+#The calculation to move the PRNG state back one frame. May be modified for other games.
+def XDRNGR(PRNG):
+    return (PRNG * 0xB9B33155 + 0xA170F641) & 0xFFFFFFFF
+
+#The subroutine to call the RNG multiple times. May be modified for other games.
+def callRNG(PRNG,direction,times):
+    state=PRNG
+    for counter in range(0,times):
+        if direction == 1:
+            state=XDRNG(state)
+        else:
+            state=XDRNGR(state)
+
+
+    return state
+
+
+#Finds the resulting seed that will happen if you add one second to the custom RTC.
+#For Gale of Darknesxs, that is a constant 40.5 million. This may be adjusted for other games.
+def nextSeed(currSeed):
+    nextSeed = currSeed + 40500000
+    if nextSeed > 4294967296:
+        nextSeed %= 4294967296
+    return nextSeed
+
+#Prolly not the best way to validate input, but it'll do
+
+def main():
+    while True:
+        try:
+            calibratedSeed=int(raw_input("Enter the seed you get at the Gale of Darkness warning screen at 1/1/2000, 12:00:00 AM. "),16)
+            if calibratedSeed > 4294967296 or calibratedSeed < 0:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print "Your input is invalid. Try again."
+
+
+
+    while True:
+        try:
+            DesiredTarget=int(raw_input("Enter the seed at which your target Pokemon is generated. If you do not know, use Calculate PiD from IVs on RNG Reporter. "),16)
+            if DesiredTarget > 4294967296 or DesiredTarget < 0:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print "Your input is invalid. Try again."
+
+
+
+    while True:
+        try:
+            threshold=int(raw_input("Enter the minimum number of frames you want to be away from your target. This program calculates initial seeds, so try not to go below 2-3000. "), 10)
+            if threshold <= 2000:
+                    print "That's too few frames away from the target. The animations before the title screen will go over your frame! Try something over 2000."
+                    raise ValueError
+                #I seriously hope this never happens but I leave it just to be sarcastic
+            elif threshold >= 4294967296:
+                print "You know the PRNG just cycles back after 2^32 frames, right? Congratulations, you hit your target."
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print "Your input is invalid. Try again."
+
+
+
+    while True:
+        try:
+            MaxFrames=int(raw_input("Enter the furthest you are willing to be from your initial seed, not counting the minimum threshold. Values below 10000 may never be hit. "),10)
+            if MaxFrames <= 10000:
+                    print "That's too close from your initial seed. Too small a number and this program may run indefinitely and never find a result, since not all seeds are possible.  Try something over 10000."
+                #I seriously hope this never happens but I leave it just to be sarcastic
+            elif MaxFrames >= 4294967296:
+                print "You know the PRNG just cycles back after 2^32 frames, right? Congratulations, you hit your target."
+            else:
+                break
+        except ValueError:
+            print "Your input is invalid. Try again."
+
+
+    print"The program will now calculate a seed near your target. This may take 20-30+ minutes depending on your settings."
+
+
+
+
+
+    #Calls XDRNG(R) for the requested distance from your target.
+    AdjustedTarget = callRNG(DesiredTarget, 2, threshold)
+
+    secondsToAdd=0
+    targetHit = 0
+
+    minutesPassed = 0
+
+    while(targetHit == 0):
+        #Setting the PRNG as the calibrated seed here is what factors the nanosecond timing issue.
+        PRNG=calibratedSeed
+        framesAway=0
+
+        #This loop ensures you don't get frames too far from your desired target.
+        for frames in range(0,MaxFrames):
+            PRNG = XDRNG(PRNG)
+            framesAway+=1
+
+            #When a target is finally hit, we're done.
+            if PRNG == AdjustedTarget:
+                targetHit = 1
+
+                break
+
+
+
+
+        #Factors the 40.5 million addition to the PRNG between each second, while not going over 2^32.
+        calibratedSeed = nextSeed(calibratedSeed)
+        secondsToAdd+=1
+
+        #Maybe change this at some point
+        if secondsToAdd % 60 == 0:
+            minutesPassed+=1
+            print "Minute(s) added to the RTC so far: ", minutesPassed, "No seed found yet. Going for the next minute."
+
+
+    #Creates a clock object with the date set to the earliest possible seed at Jan 1 2000
+    clock = datetime.datetime(2000,1,1)
+    #Adds however many seconds necessary for the seed
+    adjustedClock= clock + datetime.timedelta(seconds=secondsToAdd-1)
+
+
+    print "The following date/time will work for your current Dolphin settings: \n"
+    print(adjustedClock)
+    print "This is year/month/day and a 24 hour clock format, that may have to be adjusted accordingly for your computer.\n"
+    print "You will be ", threshold + framesAway, " frames away from your target.\n"
+    print "Due to the nanosecond issue, this will only work on your computer with the settings you have for Dolphin right now. Please read the readme for more information."
+
+while True:
+    main()
+    more = (raw_input("Type 1 and press Enter if you want to do another search, or anything else to exit.")).lower()
+
+    if more != "1":
+        break
+
+
+
---
+++ readme.txt
@@ -0,0 +1,49 @@
+Pokemon XD RTC Calculator 1.0 Beta
+By Lucas (/u/ItsProfOak AKA Collector Togami)
+
+
+Credits:
+Rising_Fog from the Dolphin dev team, for making the custom RTC feature that made this possible
+Kaphotics and Omegadonut, for figuring out XDRNG PRNG
+Zari, for researching seeding
+Admiral_Fish, for helping with testing and making a video of this
+Chimera, for additional testing
+
+Note: this is not working for Colosseum at this time. I will update when that is done.
+
+Usage instructions:
+1. Open the program with Python 2.7 (not 2.6).
+
+2. Using dolphin devmode 5.0-266 or above, go to Config, Advanced, and check the “Enable Custom RTC” box. Set the date/time to Jan 1, 2000, at midnight.
+
+
+3. Open Gale of Darkness and pause the game at the warning screen. Check the PRNG state at 004e8610 (this value may be different for non USA versions). This is your calibrated seed, factoring the nanosecond issue. (See Nanosecond Issue explanation for more information) Enter this in the program.
+
+4. Enter the seed at which your desired target is generated. If you do not know, use RNG Reporter’s 4th gen tools -> calculate PiD from IVs to find out. Note that his program does NOT factor nature locks and otherwise unhittable seeds.
+
+5. Enter the minim number of frames you want to be away from your target. The program will run XDRNG backwards that many times, then use that value as the adjusted target. The program will reject a value that is too low, as the title screen eats frames. 
+
+Furthermore, you will want to give yourself room to handle nature locks, noise, and other factors that use frames before you reach your target.
+
+6. Enter the maximum distance you want to be from your adjusted target. Let’s say your adjusted target is 10,000 frames from your target seed, and you do not want to be more than 50,000 frames away from it, from your starting seed. In that case, enter 40,000. Due to the limited amount of possible initial seeds, values that are too small will prompt the user to input a higher value.
+
+ You may change the conditional in the code itself, but too low a value may cause the program to run indefinitely and never find a target.
+
+7. Wait for the program to find the date/time for your target. This may take anywhere from a few minutes to over an hour, depending on your computer’s specs and the chosen parameters. The program will let you know every time one minute’s worth of RTC seeds passes.
+8. When a target is found, enter the date/time specified (note: this outputs the calendar in a predetermined format and may have to be adjusted for your computer, such as AM/PM vs 24 hour) in your custom RTC settings.
+9. Double- check the result by entering the seed you get from Dolphin at the given date/time. From there, RNG your target as usual.
+
+Nanosecond Issue Explanation (technical information on how/why this works):
+	Gamecube and Wii games that use date/time for seeding will do so at a nanosecond level. In the case of Gale of Darkness, the initial seed has a constant 40.5 million added to it for every second incremented on the RTC.  This allows one to predict what the next seed will be. I have left comments on the code about what parts to change, if one wishes to adapt the code for another game. This may be useful for TAS, speedrunning, debugging, and other things that require RNG manipulation. Doing so is my way of saying thanks to the Dolphin devs and the TAS community, for their help on making this possible.
+Side note: depending on how 3DS emulation turns out in the future, it may be possible to use this program to manipulate initial seeds there. I plan to revisit this when the time comes.
+
+While you can replicate the same result over and over, it is highly unlikely that two computers will obtain the same result. The following is an incomplete list of what may affect your seeding. If you know something else that may affect it, please let me know by submitting an issue or just emailing me.
+1. Incorrect custom RTC value entered (or custom RTC being disabled).
+2. Using the GC  BIOS.
+3. Different computers (due to CPU).
+4. Any setting on your computer itself that affects clock timing, such as overclocking.
+5. CPU emulation engine setting.
+6. Audio emulation setting (anything in the Audio section of the config, except volume level).
+7. CPU clock override in Dolphin.
+8. SRAM.raw file.
+9. Different versions of Dolphin.
